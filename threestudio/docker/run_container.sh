10017e7cdcc4f089842929671d01a21e430dc4e8
JustinLungu <iustinl113@gmail.com>
2025-05-25 17:35:47 +0200

    running threestudio inside foundationpose manually

--- threestudio/docker/run_container.sh
+++ threestudio/docker/run_container.sh
@@ -3,20 +3,18 @@
 # ========== CONFIGURATION ==========
 IMAGE_NAME="threestudio_custom"
 CONTAINER_NAME="threestudio"
-CACHE_DIR="/home/justin/"
 
-# Optional: directory where your Dockerfile is located
-DOCKERFILE_DIR="."
-# Optional: mount the project directory into the container
-HOST_WORKSPACE="$(cd .. && pwd)"
+# Path on the HOST where the project is located
+HOST_WORKSPACE="/home/justin/thesis/FoundationPose-BachelorThesis/threestudio"
 CONTAINER_WORKSPACE="/workspace"
+
+# HuggingFace and other caches
+HOST_CACHE_DIR="/home/justin/.cache/huggingface"
+CONTAINER_CACHE_DIR="/home/dreamer/.cache/huggingface"
 # ===================================
 
 echo "Cleaning up old container (if it exists)..."
 docker rm -f $CONTAINER_NAME 2>/dev/null || true
-
-# echo "Building Docker image..."
-# docker build -t $IMAGE_NAME $DOCKERFILE_DIR
 
 echo "Running container..."
 docker run -it --rm \
@@ -31,7 +29,8 @@ docker run -it --rm \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v $HOME/.Xauthority:/root/.Xauthority \
   -v $HOST_WORKSPACE:$CONTAINER_WORKSPACE \
-  -v $CACHE_DIR/.cache/huggingface:/home/dreamer/.cache/huggingface \
+  -v $HOST_WORKSPACE:$HOME/threestudio \
+  -v $HOST_CACHE_DIR:$CONTAINER_CACHE_DIR \
   -v /var/run/docker.sock:/var/run/docker.sock \
   $IMAGE_NAME \
   bash
